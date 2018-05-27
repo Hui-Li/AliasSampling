7928441ab3f3eda05d07629cd4cfcc71d1b70ce8
Hui Li <hli2@cs.hku.hk>
2018-05-27 15:28:54 +0800

    add STD discrete_distribution

--- main.cpp
+++ main.cpp
@@ -21,16 +21,33 @@ int main() {
     std::mt19937 gen(rd());
     std::uniform_real_distribution<> dis(0, 1.0);
 
+    std::cout << "Alias Sampling" << std::endl;
     for (int i = 0; i < 100000; i++) {
         int index = generator.sample(dis(gen), dis(gen));
         if (index > n || index < 0) {
             std::cout << index << std::endl;
         }
-        count[generator.sample(dis(gen), dis(gen))]++;
+        count[index]++;
     }
 
     for (int i = 0; i < n; i++) {
         std::cout << i << ": " << count[i] << std::endl;
+    }
+
+    std::cout << "STD discrete_distribution" << std::endl;
+    vector<int> count2(n, 0);
+
+    std::discrete_distribution<> std_generator(weight, weight + n);
+    for (int i = 0; i < 100000; i++) {
+        int index = std_generator(gen);
+        if (index > n || index < 0) {
+            std::cout << index << std::endl;
+        }
+        count2[index]++;
+    }
+
+    for (int i = 0; i < n; i++) {
+        std::cout << i << ": " << count2[i] << std::endl;
     }
 
     return 0;
